0690d8401255c2eecc77429055dc2f3e64d505de
Gio1845 <victor-reyesr@hotmail.com>
2020-02-27 10:04:01 -0700

    arreglando el random

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Guss_the_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            string num;
+            int count = 0;
+            Random r = new Random ();
+            r.NextDouble()*101;
+
+            
+            
+            Console.WriteLine("Escribe un numero del 1 al 100");
+            num = Console.ReadLine();
+            
+            
+        }
+    }
+}
