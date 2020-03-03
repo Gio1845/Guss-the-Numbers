cf00ccbb2ee4852903016cdd8e8259426dd272f8
Gio1845 <victor-reyesr@hotmail.com>
2020-03-03 14:03:20 -0700

    fix-respuesta

--- Program.cs
+++ Program.cs
@@ -15,14 +15,43 @@ namespace Guss_the_Numbers
             num = Console.ReadLine();
             num2 = Convert.ToInt32(num);
 
-             
+
             //numero de intentos
             int count = 0;
 
-            Random r = new Random ();
-            x = Convert.ToInt32(Math.Floor(r.NextDouble()*num2));
-            
-            
+            Random r = new Random();
+            x = Convert.ToInt32(Math.Floor(r.NextDouble() * num2));
+            Console.WriteLine("Adivina el numero entre el 1 y el numero que introdugiste anteriormente");
+            num = Console.ReadLine();
+            num2 = Convert.ToInt32(num);
+
+            int i = 0;
+
+            while (i == 0)
+            {
+
+                Console.WriteLine("Adivina el numero entre el 0 y el numero que introdugiste anteriormente");
+                num = Console.ReadLine();
+                num2 = Convert.ToInt32(num);
+
+                count++;
+                if (num2 > x)
+                {
+                    Console.WriteLine("el numero por adivinar es menor a ese");
+                }
+                else if (num2 < x)
+                {
+                    Console.WriteLine("el numero a adivinar es mayor a ese");
+                }
+                else
+                {
+                    Console.WriteLine("Adivinaste, felicidades");
+                    Console.WriteLine($"tu numero de intentos fue {count}");
+                    i = 1;
+                }
+
+            }
+
         }
     }
 }
